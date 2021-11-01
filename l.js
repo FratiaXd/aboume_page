521a1ec92e954b1fbe3e8300f976bfdd3a1f327b
FratiaXd <79096647+FratiaXd@users.noreply.github.com>
2021-11-01 20:13:39 +0000

    update

--- l.js
+++ l.js
@@ -1,6 +1,8 @@
 let f_i = document.getElementById('font-inc');
 let f = document.getElementById('font');
 let f_m = document.getElementById('font-min');
+let blue_b = document.getElementById('blue');
+let grey_b = document.getElementById('grey');
 
 function increaseFont(){
     f_i.setAttribute('id', 'incre')
@@ -14,6 +16,17 @@ function resetFont(){
     f_m.setAttribute('id', 'reset')
 }
 
-f_i.addEventListener('click',increaseFont);
-f.addEventListener('click',decreaseFont);
-f_m.addEventListener('click',resetFont);
+function blue(){
+    blue_b.setAttribute('id', 'make_blue')
+}
+
+function grey(){
+    grey_b.setAttribute('id', 'make_grey')
+}
+
+
+f_i.addEventListener('click', increaseFont);
+f.addEventListener('click', decreaseFont);
+f_m.addEventListener('click', resetFont);
+blue_b.addEventListener('click', blue);
+grey_b.addEventListener('click', grey);
