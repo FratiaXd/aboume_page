a092eaebe7503bfc15d3c44198003b4498279413
FratiaXd <79096647+FratiaXd@users.noreply.github.com>
2021-11-01 19:55:24 +0000

    update

--- l.js
+++ l.js
@@ -3,17 +3,17 @@ let f = document.getElementById('font');
 let f_m = document.getElementById('font-min');
 
 function increaseFont(){
-    
+    f_i.setAttribute('id', 'incre')
 }
 
 function decreaseFont(){
-    
+    f.setAttribute('id', 'decre')
 }
 
 function resetFont(){
-    
+    f_m.setAttribute('id', 'reset')
 }
 
-function increaseFont(){
-    
-}
+f_i.addEventListener('click',increaseFont);
+f.addEventListener('click',decreaseFont);
+f_m.addEventListener('click',resetFont);
