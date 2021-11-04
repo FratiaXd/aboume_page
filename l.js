eb3981b28957802b31b65258b5efdf8eab0e1605
FratiaXd <welcomcherry@gmail.com>
2021-11-04 10:33:24 +0000

    lab

--- l.js
+++ l.js
@@ -6,37 +6,58 @@ let grey_b = document.querySelector('#grey');
 let header_c = document.querySelector('#controls');
 let navi = document.querySelector('#navig');
 
-
+//grey
 grey_b.addEventListener('click', grey);
 
 function grey(){
-    header_c.setAttribute('id', 'make_grey')
+    header_c.setAttribute('id', 'make_grey');
+    localStorage.colour = 'grey';
 }
 
-
+if (localStorage.fontSize == 'grey'){
+    navi.setAttribute('id', 'make_grey');
+}
+//blue
 blue_b.addEventListener('click', blue);
 
 function blue(){
-    header_c.setAttribute('id', 'make_blue')
+    header_c.setAttribute('id', 'make_blue');
+    localStorage.colour = 'blue';
 }
 
-
+if (localStorage.fontSize == 'blue'){
+    navi.setAttribute('id', 'make_blue');
+}
+//increase
 f_i.addEventListener('click', increaseFont);
 
 function increaseFont(){
-    navi.setAttribute('id', 'incre')
+    navi.setAttribute('id', 'incre');
+    localStorage.fontSize = 'x-large';
 }
 
-
+if (localStorage.fontSize == 'x-large'){
+    navi.setAttribute('id', 'incre');
+}
+//decrease
 f.addEventListener('click', decreaseFont);
 
 function decreaseFont(){
-    navi.setAttribute('id', 'decre')
+    navi.setAttribute('id', 'decre');
+    localStorage.fontSize = 'small';
 }
 
-
+if (localStorage.fontSize == 'small'){
+    navi.setAttribute('id', 'decre');
+}
+//reset
 f_m.addEventListener('click', resetFont);
 
 function resetFont(){
-    navi.setAttribute('id', 'navig')
+    navi.setAttribute('id', 'navig');
+    localStorage.fontSize = 'normal';
+}
+
+if (localStorage.fontSize == 'normal'){
+    navi.setAttribute('id', 'navig');
 }
